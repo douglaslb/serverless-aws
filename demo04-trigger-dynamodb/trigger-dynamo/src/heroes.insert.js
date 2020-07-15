28441b803806b8eca415f29aeb1a2a08a6f7f6d7
Douglas Lima <doug.lb07@gmail.com>
2020-07-15 14:16:47 -0300

    Smart validations with Joi and DynamoDB stream events

---
+++ demo04-trigger-dynamodb/trigger-dynamo/src/heroes.insert.js
@@ -0,0 +1,79 @@
+const uuid = require('uuid')
+const Joi = require('@hapi/joi')
+const decoratorValidator = require('./util/decoratorValidator')
+const globalEnum = require('./util/globalEnum')
+
+class Handler {
+  constructor({ dynamoDbSvc }) {
+    this.dynamoDbSvc = dynamoDbSvc
+    this.dynamoDbTable = process.env.DYNAMODB_TABLE
+  }
+
+  static validator() {
+    return Joi.object({
+      nome: Joi.string().max(100).min(2).required(),
+      poder: Joi.string().max(20).required(),
+    })
+  }
+
+  async insertItem(params) {
+    return this.dynamoDbSvc.put(params).promise()
+  }
+
+  prepareData(data) {
+    const params = {
+      TableName: this.dynamoDbTable,
+      Item: {
+        ...data,
+        id: uuid.v1(),
+        createdAt: new Date().toISOString(),
+      },
+    }
+    return params
+  }
+
+  handlerSuccess(data) {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    }
+
+    return response
+  }
+
+  handlerError(data) {
+    return {
+      statusCode: data.statusCode || 501,
+      headers: { 'Content-Type': 'text/plain' },
+      body: "Couldn't create item!",
+    }
+  }
+
+  async main(event) {
+    try {
+      // agora o decorator modifica o body e já
+      // retornar no formato JSON
+      const data = event.body
+      const { error, value } = await Handler.validator().validate(data)
+
+      const dbParams = this.prepareData(data)
+      await this.insertItem(dbParams)
+      return this.handlerSuccess(dbParams.Item)
+    } catch (error) {
+      console.log('Deu ruim**', error.stack)
+      return this.handlerError({ statusCode: 500 })
+    }
+  }
+}
+
+//factory
+const AWS = require('aws-sdk')
+const dynamoDB = new AWS.DynamoDB.DocumentClient()
+const handler = new Handler({
+  dynamoDbSvc: dynamoDB,
+})
+module.exports = decoratorValidator(
+  handler.main.bind(handler),
+  Handler.validator(),
+  globalEnum.ARG_TYPE.BODY
+)
