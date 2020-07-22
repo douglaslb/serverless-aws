9ed8f8d6f1ccfdf8824e9082209388fa907f1f3f
Douglas Lima <doug.lb07@gmail.com>
2020-07-21 22:28:38 -0300

    Processing files on demand with S3 Offline and Node.js Streams

---
+++ demo08-sqs-s3-streams/sqs-s3-streams/scripts/s3/upload-file.sh
@@ -0,0 +1,10 @@
+BUCKET_NAME=$1
+FILE_PATH=$2
+
+aws \
+  s3 cp $FILE_PATH s3://$BUCKET_NAME \
+  # --endpoint-url=http://localhost:4572
+
+aws \
+  s3 ls s3://$BUCKET_NAME \
+  # --endpoint-url=http://localhost:4572
---
+++ demo08-sqs-s3-streams/sqs-s3-streams/scripts/sqs/send-message.sh
@@ -0,0 +1,15 @@
+QUEUE_URL=$1
+echo 'Sending message' $QUEUE_URL
+
+aws \
+  sqs send-message \
+  --queue-url $QUEUE_URL \
+  --message-body 'Hey ho lets go' \
+  # --endpoint-url=http://localhost:4576
+
+aws \
+  sqs receive-message \
+  --queue-url $QUEUE_URL \
+  # --endpoint-url=http://localhost:4576
+
+  
